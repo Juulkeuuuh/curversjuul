632e078ae1cb6f0dc77075ab0dfecd749e11d366
Juulkeuuuh <90319980+Juulkeuuuh@users.noreply.github.com>
2023-07-03 00:01:42 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -8,8 +8,12 @@ $(document).ready(function(){
   // });
 
 
+  var lastScrollTop = 0;
 
   $(window).on('scroll', function() {
+    var windowHeight = $(window).height();
+    var scrollPosition = $(window).scrollTop();
+
       $('.arrowDown').fadeOut(500, function() {
         $(this).remove();
       });
@@ -32,9 +36,39 @@ $(document).ready(function(){
         $('.headlogo').css('display', 'block');
 
       }
+
+
+    $('.slide-in').each(function() {
+      var offsetTop = $(this).offset().top;
+      if (scrollPosition > offsetTop - windowHeight + 300) {
+        $(this).addClass('slide-in-active');
+      }
+    });
+    // var lastScrollTop = 0;
+    // var scrollTop = $(this).scrollTop();
+    if (scrollTop > lastScrollTop) {
+      // Scrolling down
+      var newMarginTop = parseInt($('.sticky-div').css('margin-top')) - 15;
+      $('.sticky-div').css('margin-top', newMarginTop + 'px');
+    } else if(scrollTop < lastScrollTop){
+      // Scrolling up
+      var newMarginTop = parseInt($('.sticky-div').css('margin-top')) + 15;
+      $('.sticky-div').css('margin-top', newMarginTop + 'px');
+    }
+    lastScrollTop = scrollTop;
+
+    console.log(lastScrollTop, scrollTop);
   })
 
-
+  // modal
+  $('.art').click(function () {
+    var dehetid = $(this).attr('id');
+    $('.article-top-inside').load('assets/arts/'+dehetid+'.html');
+    $('.article-top').css('bottom', '0')
+  })
+  $('.close-button').click(function () {
+    $('.article-top').css('bottom', '-100%')
+  })
 
 
 })
